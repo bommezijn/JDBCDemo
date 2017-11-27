2fb372f56a93abcf73ce2ba131d8fcdc449ca973
Satan <nathanbommezijn@gmail.com>
2017-11-27 16:01:05 +0100

    Initial Commit
    Push the project to github.

---
+++ src/main/java/JDBCDemo.java
@@ -0,0 +1,15 @@
+/**
+ * Demonstrates use of JDBC in a plain Java Program
+ * @author somej
+ */
+public class JDBCDemo {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // test and demo the JDBC functionality
+        MyJDBC.createTestDatabase("bommezij_Corendon");
+    }
+}
